41da0aeeebd4ad7e63da763c0d86cf4a7dabf764
hvandang@outlook.com <hvandang@outlook.com>
2020-05-28 15:01:53 +0100

    feat(read a folder and convert into a list of jobs)

---
+++ src/main/java/json/JsonGenerator.java
@@ -0,0 +1,127 @@
+package json;
+
+import java.util.List;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class JsonGenerator {
+	@SuppressWarnings("unchecked")
+	 public static void main( String[] args )
+	    {
+		
+			File folder = new File("../data/1000files/");
+			int step = 5; //the number of files for each job
+			File[] listOfFiles = folder.listFiles(new FilenameFilter() {
+		        @Override
+		        public boolean accept(File dir, String name) {
+		            return !name.equals(".DS_Store");
+		        }
+		    }); //set filter to avoid ".DS_Store" file
+			int noFile = listOfFiles.length;
+			System.out.println("Number of files: " + noFile);
+			
+			
+			List<String> cmdList = new ArrayList<String>();
+			cmdList.add("./execute.sh");
+	        
+	        JSONArray jobList = new JSONArray();
+
+	        int count=0;
+			for (int i = 0; i < listOfFiles.length; i=i+step) {
+				List<String> sList = new ArrayList<String>();
+				sList.add(Integer.toString(step));
+				int k = i;
+		        JSONArray taskList = new JSONArray();
+				while(k < i + step && k < noFile) {
+				//for (int j=0; j<step; j++) {
+					//System.out.println(k + "\n");
+					if (listOfFiles[k].isFile()) {
+						  String fname = listOfFiles[k].getName();
+						  char slash = 92;
+						  String replacement = String.format("%c ",slash);
+						 // System.out.println(charSeq);
+						  String new_fname = fname.replace(" ",replacement);
+						  //System.out.println(new_fname);
+					      sList.add(new_fname);
+					}	
+					k = k+1;
+				}
+				//System.out.println("number of files" + sList.size());
+				/*
+				if(k < noFile) {
+					sList.add(0,Integer.toString(step));
+				}
+				else {
+					if(noFile%step!=0) {
+						sList.add(0,Integer.toString(k%step));
+					}
+					else {
+						sList.add(0,Integer.toString(step));
+					}
+				}*/
+				
+				JSONObject task = new JSONObject();
+				task.put("data",sList);
+				task.put("id","task_id_0");
+				task.put("command",cmdList);
+				//System.out.println(task);
+				taskList.add(task);
+				
+				JSONObject job = new JSONObject();
+				job.put("data", new JSONArray());
+				job.put("params",new JSONArray());
+				String job_id = "job_id_" + Integer.toString(count);
+				count++;
+				job.put("id", job_id);
+				job.put("command",new JSONArray());
+				job.put("tasks",taskList);
+			      
+				jobList.add(job);
+			}
+	        
+	       
+	        
+	       /*
+	        
+	        JSONObject task2 = new JSONObject();
+	        List<String> sList2 = new ArrayList<String>();
+	        sList2.add("2.txt");
+	        task2.put("data",sList2);
+	        task2.put("id","task_id_0");
+	        task2.put("command",cmdList);
+	        
+	        JSONObject job2 = new JSONObject();
+	        job2.put("data", new JSONArray());
+	        job2.put("params",new JSONArray());
+	        job2.put("id", "job_id_1");
+	        job2.put("command",cmdList);
+	        job2.put("tasks",task2);*/
+	        
+
+	        
+	        
+	        JSONObject jobsDescription = new JSONObject();
+	        jobsDescription.put("container_name", "worker");
+	        jobsDescription.put("experiment_deadline", 3895);
+	        jobsDescription.put("single_task_duration", 60);
+	        jobsDescription.put("params",new JSONArray());
+	        jobsDescription.put("command",new JSONArray());
+	        jobsDescription.put("jobs",jobList);
+	         
+	        //Write JSON file
+	        try (FileWriter file = new FileWriter("sse-testing.json")) {
+	 
+	            file.write(jobsDescription.toJSONString());
+	            file.flush();
+	 
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+}
